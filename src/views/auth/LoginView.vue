0fc015f019e82b878e9da51b12e022c47c88c94a
ccariate <cindycariate@gmail.com>
2024-11-27 14:17:35 +0700

    feat: fixing login view

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
 import BackgroundLayout from '@/components/auth/BackgroundLayout.vue'
 
-const theme = ref('dark')
+const theme = ref('light')
 
 function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'dark'
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
 }
 </script>
 
@@ -14,65 +15,83 @@ function onClick() {
     <v-responsive>
       <v-app :theme="theme" class="transparent-app">
         <v-app-bar class="px-3" color="transparent">
+          <h3 class="text-logo font-weight-black">TaskHub</h3>
           <v-spacer></v-spacer>
 
-          <v-btn
-            :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-            slim
-            @click="onClick"
-          ></v-btn>
+          <!-- <v-btn 
+          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          text="Toggle Theme"
+          slim
+          @click="onClick"
+        ></v-btn>-->
         </v-app-bar>
 
         <v-main>
-          <v-container class="transparent-container">
-            <v-row class="d-flex justify-center align-center" style="min-height: 100vh">
+          <v-container>
+            <v-row justify="center" align-content="center">
               <v-col cols="12" md="6">
-                <h1 class="text-center text-h2" color="">taskHub</h1>
-                <h4 class="text-white justify-content-center py-8 text-h6">
+                <v-img
+                  class="mx-auto"
+                  :width="290"
+                  aspect-ratio="16/9"
+                  cover
+                  src="/public/logo_icon.png"
+                  alt="TaskHub"
+                ></v-img>
+
+                <p class="description text-center">
                   A Task Manager app helps users create tasks, assign priorities, and monitor
                   progress to stay organized and meet deadlines efficiently.
-                </h4>
-              </v-col>
-
-              <v-col cols="12" md="6" class="d-flex justify-center align-center py-8 mx-auto">
-                <v-card class="glassmorphic-card" elevation="16" max-width="800">
+                </p></v-col
+              >
+              <v-col cols="12" md="6" class="mt-13">
+                <v-card
+                  class="glassmorphic-card mx-auto"
+                  :max-width="'500'"
+                  elevation="14"
+                  rounded="lg"
+                >
                   <template v-slot:title>
-                    <span class="text-center d-block font-weight-white"><h2>LOGIN</h2></span>
+                    <span class="font-weight-black text-center d-block text-uppercase pt-4"
+                      >login</span
+                    >
                   </template>
 
-                  <v-card-text class="pt-3">
+                  <v-card-text class="pt-4">
                     <v-form fast-fail @submit.prevent>
-                      <v-text-field label="Email" variant="outlined">
-                        <template v-slot:label>
-                          <!-- <v-icon class="mr-1">mdi-email</v-icon>-->
-                          Email
-                        </template>
-                      </v-text-field>
+                      <v-text-field
+                        label="Email"
+                        variant="outlined"
+                        prepend-inner-icon="mdi-email"
+                      ></v-text-field>
 
-                      <v-text-field label="Password" type="password" variant="outlined">
-                        <template v-slot:label>
-                          <!-- <v-icon class="mr-1">mdi-lock</v-icon> -->
-                          Password
-                        </template>
-                      </v-text-field>
+                      <v-text-field
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        prepend-inner-icon="mdi-lock"
+                      ></v-text-field>
 
-                      <v-btn class="mt-2 rounded" color="secondary" type="submit" block
-                        >Login</v-btn
+                      <v-btn
+                        class="mt-2 text-white"
+                        type="submit"
+                        color="cyan-lighten-2"
+                        rounded="xl"
+                        block
+                        >Log in</v-btn
                       >
                     </v-form>
                     <v-divider class="my-5"></v-divider>
                     <h4 class="text-center">
                       Don't have account?
-                      <RouterLink class="text-white" to="/signUp"><b>Sign up</b></RouterLink>
+                      <RouterLink style="color: white" to="/signup"><b>Sign up</b></RouterLink>
                     </h4>
                   </v-card-text>
-                </v-card></v-col
-              >
+                </v-card>
+              </v-col>
             </v-row>
           </v-container>
         </v-main>
-
-        <!-- <v-footer color="transparent">2024-Copyright TaskHub</v-footer> -->
       </v-app>
     </v-responsive>
   </BackgroundLayout>
@@ -86,11 +105,24 @@ function onClick() {
   color: white;
   background: rgba(64, 64, 64, 0.15); /* Transparent white */
   backdrop-filter: blur(25px); /* Frosted glass effect */
-  -webkit-backdrop-filter: blur(10px); /* Safari support */
-  border: 2px solid rgba(255, 255, 255, 0.826); /* Frosted border */
+  /* -webkit-backdrop-filter: blur(10px); Safari support */
+  border: 2px solid rgba(255, 255, 255, 0.959); /* Frosted border */
   border-radius: 16px; /* Rounded corners */
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Shadow for depth */
-  padding: 20px; /* Optional padding for inner spacing */
-  width: 70%;
+}
+
+.description {
+  font-size: 18px;
+  color: white;
+  text-align: center;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva,
+    Verdana, sans-serif;
+}
+
+.text-logo {
+  color: aqua;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-weight: bolder;
+  font-size: x-large;
 }
 </style>
