c4c5c6ba1ff64d565b16a707841d52e45f12822b
ccariate <cindycariate@gmail.com>
2024-11-27 14:18:45 +0700

    feat: fixing sign up view

--- src/views/auth/SignUpView.vue
+++ src/views/auth/SignUpView.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import BackgroundLayout from '@/components/auth/BackgroundLayout.vue'
 
-const theme = ref('dark')
+const theme = ref('light')
 
 function onClick() {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
@@ -14,60 +14,98 @@ function onClick() {
     <v-responsive>
       <v-app :theme="theme" class="transparent-app">
         <v-app-bar class="px-3" color="transparent">
+          <h3 class="text-logo font-weight-black">TaskHub</h3>
+
           <v-spacer></v-spacer>
 
-          <v-btn
-            :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-            slim
-            @click="onClick"
-          ></v-btn>
+          <!-- <v-btn 
+          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          text="Toggle Theme"
+          slim
+          @click="onClick"
+        ></v-btn>-->
         </v-app-bar>
 
         <v-main>
-          <v-container class="transparent-container">
-            <v-row class="d-flex justify-center align-center" style="min-height: 100vh">
+          <v-container class="d-flex justify-center align-center my-4">
+            <v-row justify="center" align-content="center" no-gutters="">
               <v-col cols="12" md="6">
-                <h1 class="text-center text-h2" color="">taskHub</h1>
-                <h4 class="text-white justify-content-center py-8 text-h6">
+                <v-img
+                  class="mx-auto"
+                  :width="290"
+                  aspect-ratio="16/9"
+                  cover
+                  src="/logo_icon.png"
+                  alt="TaskHub"
+                ></v-img>
+                <p class="description text-center mb-5">
                   A Task Manager app helps users create tasks, assign priorities, and monitor
                   progress to stay organized and meet deadlines efficiently.
-                </h4>
-              </v-col>
-
-              <v-col cols="12" md="6" class="d-flex justify-center align-center py-8">
-                <v-card class="glassmorphic-card" elevation="16" max-width="900">
+                </p></v-col
+              >
+              <v-col cols="12" md="6">
+                <v-card
+                  class="glassmorphic-card mx-auto"
+                  :max-width="'500'"
+                  elevation="14"
+                  rounded="lg"
+                >
                   <template v-slot:title>
-                    <span class="text-center d-block font-weight-white"><h2>SIGN UP</h2></span>
+                    <span class="font-weight-black text-center d-block text-uppercase pt-4"
+                      >sign up</span
+                    >
                   </template>
 
-                  <v-card-text class="pt-3">
+                  <v-card-text class="pt-4">
                     <v-form fast-fail @submit.prevent>
-                      <v-text-field label="First name" variant="outlined"> </v-text-field>
-                      <v-text-field label="Last name" variant="outlined"> </v-text-field>
-                      <v-text-field label="Email" variant="outlined"> </v-text-field>
+                      <v-row>
+                        <v-col cols="12" sm="6">
+                          <v-text-field label="First name" variant="outlined"></v-text-field
+                        ></v-col>
+                        <v-col cols="12" sm="6">
+                          <v-text-field label="Last name" variant="outlined"></v-text-field
+                        ></v-col>
+                      </v-row>
 
-                      <v-text-field label="Password" type="password" variant="outlined">
-                      </v-text-field>
-                      <v-text-field label="Confirm Password" type="password" variant="outlined">
-                      </v-text-field>
+                      <v-text-field
+                        label="Email"
+                        variant="outlined"
+                        prepend-inner-icon="mdi-email"
+                      ></v-text-field>
 
-                      <v-btn class="mt-2 rounded" color="secondary" type="submit" block
+                      <v-text-field
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        prepend-inner-icon="mdi-lock"
+                      ></v-text-field>
+                      <v-text-field
+                        label="Confirm Password"
+                        type="password"
+                        variant="outlined"
+                        prepend-inner-icon="mdi-lock"
+                      ></v-text-field>
+
+                      <v-btn
+                        class="mt-2 text-white"
+                        type="submit"
+                        color="cyan-lighten-3"
+                        rounded="xl"
+                        block
                         >Sign up</v-btn
                       >
                     </v-form>
                     <v-divider class="my-5"></v-divider>
                     <h4 class="text-center">
                       Already have an account?
-                      <RouterLink class="text-white" to="/"><b>Login</b></RouterLink>
+                      <RouterLink style="color: white" to="/"><b>Login</b></RouterLink>
                     </h4>
                   </v-card-text>
-                </v-card></v-col
-              >
+                </v-card>
+              </v-col>
             </v-row>
           </v-container>
         </v-main>
-
-        <!-- <v-footer color="transparent">2024-Copyright TaskHub</v-footer> -->
       </v-app>
     </v-responsive>
   </BackgroundLayout>
@@ -77,15 +115,25 @@ function onClick() {
 .transparent-app {
   background: transparent;
 }
+
 .glassmorphic-card {
   color: white;
   background: rgba(64, 64, 64, 0.15); /* Transparent white */
-  backdrop-filter: blur(25px); /* Frosted glass effect */
-  -webkit-backdrop-filter: blur(10px); /* Safari support */
-  border: 2px solid rgba(255, 255, 255, 0.826); /* Frosted border */
-  border-radius: 16px; /* Rounded corners */
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Shadow for depth */
-  padding: 20px; /* Optional padding for inner spacing */
-  width: 70%;
+  backdrop-filter: blur(25px);
+  border: 2px solid rgba(255, 255, 255, 0.959); /* Frosted border */
+}
+.description {
+  font-size: 18px;
+  color: white;
+  text-align: center;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva,
+    Verdana, sans-serif;
+}
+
+.text-logo {
+  color: aqua;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-weight: bolder;
+  font-size: x-large;
 }
 </style>
