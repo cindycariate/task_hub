61687b5c12e2b7aa2ddb9788bf29bc58108234dc
Lazeon29 <sophiacalimbo@gmail.com>
2024-12-17 14:37:16 +0800

    fix: changed the color in TaskView.vue

--- src/components/pages/TaskView.vue
+++ src/components/pages/TaskView.vue
@@ -6,7 +6,7 @@ import { ref } from 'vue'
 const isDrawerVisible = ref(true)
 
 // for the tabs part
-const tab = ref(null)
+const tab = ref(1)
 
 // Toggle function for the sidebar
 const toggleSidebar = () => {
@@ -18,9 +18,23 @@ const toggleSidebar = () => {
   <AppLayout :is-with-app-bar-nav-icon="true" :is-drawer-visible="isDrawerVisible">
     <template #content>
       <v-container fluid>
-        <h1>TaskHub</h1>
-        <h2>Hello</h2>
-        <!-- Your Task Content Goes Here -->
+        <v-card>
+          <v-tabs v-model="tab" class="auth-background tabs-head">
+            <v-tab value="one">To Do</v-tab>
+            <v-tab value="two">In Progress</v-tab>
+            <v-tab value="three">Done</v-tab>
+          </v-tabs>
+
+          <v-card-text>
+            <v-tabs-window v-model="tab">
+              <v-tabs-window-item value="one"> One </v-tabs-window-item>
+
+              <v-tabs-window-item value="two"> Two </v-tabs-window-item>
+
+              <v-tabs-window-item value="three"> Three </v-tabs-window-item>
+            </v-tabs-window>
+          </v-card-text>
+        </v-card>
       </v-container>
     </template>
   </AppLayout>
@@ -48,9 +62,22 @@ const toggleSidebar = () => {
   left: 20px;
   z-index: 10;
 }
-
+.auth-background {
+  background: linear-gradient(140deg, #0a0a0b, #1ea8b0);
+  /* background-image: url('@/assets/img/bg.jpg'); */
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
 .toggle-btn {
   background-color: rgba(0, 0, 0, 0.2);
   color: white;
+}
+.tabs-head {
+  color: white;
+  font-family: 'Poppins', sans-serif;
 }
 </style>
