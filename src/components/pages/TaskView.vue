c8ce74d67b3e51eed91974d4a056a0ab2a32effa
ccariate <cindycariate@gmail.com>
2024-12-17 14:02:44 +0700

    fix:fixing task view

--- src/components/pages/TaskView.vue
+++ src/components/pages/TaskView.vue
@@ -1,27 +1,19 @@
+<!-- TaskView.vue -->
 <script setup>
 import AppLayout from '../layout/AppLayout.vue'
-import SideNav from '../layout/navigation/SideNav.vue'
 import { ref } from 'vue'
+
 const isDrawerVisible = ref(true)
 </script>
 
 <template>
-  <AppLayout
-    :is-with-app-bar-nav-icon="true"
-    @is-drawer-visible="isDrawerVisible = !isDrawerVisible"
-  >
-    <template #navigation> </template>
+  <AppLayout :is-with-app-bar-nav-icon="true" :is-drawer-visible="isDrawerVisible">
     <template #content>
-      <v-container-fluid class="container">
-        <!-- Main Content -->
-        <div class="menu-btn">
-          <!-- Button to toggle the sidebar visibility -->
-          <v-btn icon @click="toggleSidebar" class="toggle-btn">
-            <v-icon>{{ isDrawerVisible ? 'mdi-menu-open' : 'mdi-menu' }}</v-icon>
-          </v-btn>
-        </div>
-        <div class="main-content"></div
-      ></v-container-fluid>
+      <v-container fluid>
+        <h1>TaskHub</h1>
+        <h2>Hello</h2>
+        <!-- Your Task Content Goes Here -->
+      </v-container>
     </template>
   </AppLayout>
 </template>
--- src/views/system/DashboardView.vue
+++ src/views/system/DashboardView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AppLayout from '@/components/layout/AppLayout.vue'
-import SideNavigation from '@/components/layout/navigation/SideNavigation.vue'
+import SideNav from '@/components/layout/navigation/SideNav.vue'
 import { ref } from 'vue'
 
 const isDrawerVisible = ref(true)
@@ -11,29 +11,8 @@ const isDrawerVisible = ref(true)
     :is-with-app-bar-nav-icon="true"
     @is-drawer-visible="isDrawerVisible = !isDrawerVisible"
   >
-    <template #navigation> </template>
-    <template #content>
-      <v-container-fluid class="container">
-        <!-- Main Content -->
-        <div class="menu-btn">
-          <!-- Button to toggle the sidebar visibility -->
-          <v-btn icon @click="toggleSidebar" class="toggle-btn">
-            <v-icon>{{ isDrawerVisible ? 'mdi-menu-open' : 'mdi-menu' }}</v-icon>
-          </v-btn>
-        </div>
-        <div class="main-content">
-          <div class="welcome-message">
-            <h1 class="text-white text-h3">
-              Welcome to <span class="task-text">Task</span><span class="hub-text">Hub</span>
-            </h1>
-            <p class="p">Visually Manage Your Tasks</p>
-            <button class="create-new-btn rounded-pill">
-              <i class="mdi mdi-plus"></i> Create New
-            </button>
-          </div>
-        </div></v-container-fluid
-      >
-    </template>
+    <template #navigation> <SideNav const isDrawerVisible="ref(true)"></SideNav> </template>
+    <template #content> </template>
   </AppLayout>
 </template>
 
